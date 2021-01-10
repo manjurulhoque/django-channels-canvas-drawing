c60b4ae2ccc060a96d3d8613e3746755940745dc
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-01-11 00:08:15 +0600

    Core app created

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vg9tt%-fr8mu(ixj=*c3fzma2ag8*t2d%0kerwdeg5*c8eb4qk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
